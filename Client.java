252014263eb596282840f0e21c67fb94250d4f1a
nmerkibek <nurlyaimerkibek@gmail.com>
2024-11-28 17:56:33 +0500

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,39 +1,59 @@
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 
-public class Client {
+abstract class Shape implements Serializable {
+    public abstract double calculateArea();
+
+}
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+}
+
+public class Server {
     public static void main(String[] args) {
-        try (Socket socket = new Socket("localhost", 5000);
-             ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-             ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-             Scanner scanner = new Scanner(System.in)) {
-            while (true) {
-                System.out.println("введите фигуру или 'Q' чтобы выйти:");
-                String command = scanner.nextLine();
-
-                if (command.equalsIgnoreCase("Q")) {
-                    output.writeObject("Q");
-                    break;
-                } else if (command.equalsIgnoreCase("круг")) {
-                    System.out.println("введите радиус:");
-                    double radius = scanner.nextDouble();
-                    scanner.nextLine();
-                    output.writeObject(new Circle(radius));
-                } else if (command.equalsIgnoreCase("прямоугольник")) {
-                    System.out.println("введите ширину и высоту:");
-                    double width = scanner.nextDouble();
-                    double height = scanner.nextDouble();
-                    scanner.nextLine();
-                    output.writeObject(new Rectangle(width, height));
+        try (ServerSocket Socket = new ServerSocket(4900)) {
+            System.out.println("сервер запущен...");
+            try (Socket socket = Socket.accept();
+                 ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+                 ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream())) {
+                while (true) {
+                    Object obj = input.readObject();
+                    if (obj instanceof Shape) {
+                        Shape shape = (Shape) obj;
+                        double area = shape.calculateArea();
+                        output.writeObject("Ответ будет:\n " + area + " вот!");
+                    } else if (obj.equals("Q")) {
+                        System.out.println("соединение завершено...");
+                        break;
+                    }
                 }
-
-                String response = (String) input.readObject();
-                System.out.println(response);
             }
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 }
-
