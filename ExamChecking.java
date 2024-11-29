7e8002e57b5140a47879184a77729ea77b6fc6e9
nmerkibek <nurlyaimerkibek@gmail.com>
2024-11-29 14:42:37 +0500

    Update ExamChecking.java

--- ExamChecking.java
+++ ExamChecking.java
@@ -1,10 +1,9 @@
 package ExamChecker;
-
-import java.util.concurrent.atomic.AtomicInteger;
+import java.time.LocalDateTime;
 
 class ExamChecking extends Thread {
     private static final int TOTAL_SHEETS = 500;
-    private static AtomicInteger examSheets = new AtomicInteger(TOTAL_SHEETS);
+    private static int examSheets = TOTAL_SHEETS;
     private int limit;
 
     public ExamChecking(int limit) {
@@ -15,19 +14,22 @@ class ExamChecking extends Thread {
     public void run() {
         for (int i = 0; i < limit; i++) {
             synchronized (ExamChecking.class) {
-                if (examSheets.get() <= 0) {
-                    System.out.println(" все документы проверились и листов не осталось!");
+                if (examSheets <= 0) {
+                    System.out.println("Все документы проверены и листов не осталось");
                     System.exit(0);
                 }
-                int remaining = examSheets.addAndGet(-50);
+
+                examSheets -= 50;
+
                 System.out.println(Thread.currentThread().getName() + " закончил(-а) проверку в: " +
-                        java.time.LocalDateTime.now() +
-                        " количество экзаменационных листов на данный момент: " + remaining);
+                        LocalDateTime.now() +
+                        " кол-во экз листов на данный момент: " + examSheets);
+
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
-                    System.out.println("thread прервано: " + e.getMessage());
+                    System.out.println("поток прерван: " + e.getMessage());
                 }
             }
         }
